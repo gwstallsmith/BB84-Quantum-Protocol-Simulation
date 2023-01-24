d8f49f07e8b4eb0ac8af01dd78263239090f6edc
Garrett <gwstallsmith@gmail.com>
2023-01-24 17:39:00 -0500

    qubit class

--- bb84.cpp
+++ bb84.cpp
@@ -9,14 +9,32 @@ using std::pair;
 
 class Qubit {
 public:
-    Qubit() : bit_('0'), state_('+') {}
+    Qubit() : bit_('0'), state_('+'), tensor_(bit_, state_) {}
 
     char getBit(){ return bit_; }
     char getState() { return state_; }
+    pair<char, char> getTensor() { return tensor_; }
 
     void setBit(char newBit) {
+        if(newBit != '0' || '1')
+            return;
+        bit_ = newBit;
     }
-    void setState() {}
+    void setState(char newState) {
+        if(newState != '+' || 'x')
+            return;
+        state_ = newState;
+    }
+    void setTensor(char newBit, char newState) {
+        if((newBit != '0' || '1') || (newState != '+' || 'x'))
+            return;
+
+        setBit(newBit);
+        setState(newState);
+
+        tensor_.first = newBit;
+        tensor_.second = newState;
+    }
 
 private:
     char bit_;
@@ -24,7 +42,39 @@ private:
     pair<char, char> tensor_;
 };
 
-class Alice {};
+class Alice {
+public:
+    Alice() : n_(0), privKey_({}) {}
+
+    vector<Qubit> generateKey(int size) {
+        n_ = randN(size);
+        privKey_.resize(n_);
+
+        for(auto q : privKey_) {
+            q.setTensor(randBit(), randState());
+        }
+    }
+
+    int randN(int size) { return rand() % size;}
+
+    char randBit() {
+        if(rand() % 2 == 0)
+            return '+';
+        else
+            return 'x';
+    }
+    char randState() {
+        if(rand() % 2 == 0)
+            return '0';
+        else
+            return '1';
+    }
+
+
+private:
+    int n_;    
+    vector<Qubit> privKey_;
+};
 
 class Bob {};
 
