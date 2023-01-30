13191f6b547477e9c47d6f25f6d539bab0a4c3ed
Garrett <gwstallsmith@gmail.com>
2023-01-30 17:57:51 -0500

    Bob now measures Qubits with 50% accuracy.

--- bb84.cpp
+++ bb84.cpp
@@ -8,7 +8,7 @@ using std::string; using std::vector;
 
 class Qubit {
 public:
-    Qubit() : bit_("0"), basis_("+"), polar_("0") {}
+    Qubit() : bit_("1"), basis_("+"), polar_("0") {}
 
     Qubit(string bit, string basis, string polar){
         bit_ = bit;
@@ -47,52 +47,62 @@ private:
 
 class Alice {
 public:
-    Alice() : basis_("+"), otp_({}), botp_({}) {}
+    Alice() : bit_("0"), basis_("+"), otp_({}), botp_({}) {}
 
     vector<Qubit> generateOTP(int size) {
         otp_.resize(size);
         for(auto &q : otp_) {
-            randBasisPolar();
-            q = Qubit(randBit(), basis_, polar_);
+            randBasisPolarBit();
+            q = Qubit(bit_, basis_, polar_);
         }
         return otp_;
     }
 
     string randBit() { if(rand() % 2 == 0) return "0"; else return "1"; }
 
-    void randBasisPolar() {
+    void randBasisPolarBit() {
         if(rand() % 2 == 0) {
             basis_ = "+";
-            if(rand() % 2 == 0)
+            if(rand() % 2 == 0) {
                 polar_ = "0";
-            else
+                bit_ = "1";
+            }
+            else {
                 polar_ = "90";
+                bit_ = "0";
+            }
         } else {
             basis_ = "x";
-            if(rand() % 2 == 0)
+            if(rand() % 2 == 0) {
                 polar_ = "45";
-            else
+                bit_ = "0";
+            }
+            else {
                 polar_ = "135";
+                bit_ = "1";
+            }
         }
     }
 
     vector<string> compareOTP(vector<Qubit> inOTP){
         for(int i = 0; i < inOTP.size(); i++) {
             if(otp_[i] == inOTP[i]){
-                botp_.push_back(inOTP[i].getBit());
+                botp_.push_back(otp_[i].getBit());
             }
         }
         return botp_;
     }
 
-    void printOTP() { for(auto &q : otp_) { cout << q.getBit() << " " << q.getBasis() << " " << q.getPolar() << endl; } }
 
-    void printBOTP() { for(auto &str: botp_) { cout << str << endl; } }
+    void printOTP() { int i = 0; for(auto &q : otp_) { cout << i++ << ".) "; cout << q.getBit() << " " << q.getBasis() << " " << q.getPolar() << endl; } }
+
+    void printBOTP() { int i = 0; for(auto &str: botp_) { cout << i++ << ".) "; cout << str << endl; } }
 
 
 private:
     // One Time Pad / Shared Key
     // otp_ is a cleaner name
+    string bit_;
     string basis_;
     string polar_;
     vector<Qubit> otp_;
@@ -102,44 +112,69 @@ private:
 
 class Bob {
 public:
-    Bob() : basis_("+"), polar_("0"), otp_({}), botp_({}) {}
+    Bob() : bit_("0"), basis_("+"), polar_("0"), otp_({}), botp_({}) {}
 
     vector<Qubit> measureOTP(vector<Qubit> inOTP) {
         otp_.resize(inOTP.size());
 
         for(int i = 0; i < inOTP.size(); i++) {
-            randBasisPolar();
+            randBasisPolarBit();
+
             if(inOTP[i].getBasis() == basis_ && inOTP[i].getPolar() == polar_)
                 otp_[i] = inOTP[i];
-            else
+
+            else if(inOTP[i].getBasis() == basis_ && inOTP[i].getPolar() != polar_) {
+                if(inOTP[i].getPolar() == "0")
+                    otp_[i] = Qubit("1", "+", "0");
+
+                else if(inOTP[i].getPolar() == "45")
+                    otp_[i] = Qubit("0", "x", "45");
+
+                else if(inOTP[i].getPolar() == "90")
+                    otp_[i] = Qubit("0", "+", "90");
+
+                else if(inOTP[i].getPolar() == "135")
+                    otp_[i] = Qubit("1", "x", "135");
+
+            } else
                 otp_[i] = Qubit("U", basis_, polar_);
+
         }
         return otp_;
     }
 
-    void randBasisPolar() {
+    void randBasisPolarBit() {
         if(rand() % 2 == 0) {
             basis_ = "+";
-            if(rand() % 2 == 0)
+            if(rand() % 2 == 0) {
                 polar_ = "0";
-            else
+                bit_ = "1";
+            }
+            else {
                 polar_ = "90";
+                bit_ = "0";
+            }
         } else {
             basis_ = "x";
-            if(rand() % 2 == 0)
+            if(rand() % 2 == 0) {
                 polar_ = "45";
-            else
+                bit_ = "0";
+            }
+            else {
                 polar_ = "135";
+                bit_ = "1";
+            }
         }
     }
 
 
-    void printOTP() { for(auto &q : otp_) { cout << q.getBit() << " " << q.getBasis() << " " << q.getPolar() << endl; } }
+    void printOTP() { int i = 0; for(auto &q : otp_) { cout << i++ << ".) "; cout << q.getBit() << " " << q.getBasis() << " " << q.getPolar() << endl; } }
 
-    void printBOTP() { for(auto &str: botp_) { cout << str << endl; } }
+    void printBOTP() { int i = 0; for(auto &str: botp_) { cout << i++ << ".) "; cout << str << endl; } }
 
 
 private:
+    string bit_;
     string basis_;
     string polar_;
     vector<Qubit> otp_;
@@ -149,27 +184,28 @@ private:
 class Eve {};
 
 
-
 int main() {
     srand(time(0));
     Alice a;
     Bob b;
 
+    int size = 1000;
     vector<string> agreedOTP;
 
-    agreedOTP = a.compareOTP(b.measureOTP(a.generateOTP(10)));
+    agreedOTP = a.compareOTP(b.measureOTP(a.generateOTP(size)));
 
-    cout << "Alice sends..." << endl;
+//    cout << endl << "Alice sends..." << endl;
+//
+//    a.printOTP();
+//
+//    cout << endl << "Bob measures..." << endl;
+//
+//    b.printOTP();
+//
+//    cout << endl << "Agreed OTP after comparison..." << endl;
+//
+//    a.printBOTP();
+//
+    cout << endl << "Amount of Bits: " << agreedOTP.size() << " / " << size << endl;
 
-    a.printOTP();
-
-    cout << endl << "Bob measures..." << endl;
-
-    b.printOTP();
-
-    cout << endl << "Agreed OTP after comparison..." << endl;
-
-    a.printBOTP();
-
-    return 0;
 }
