dd3955aa784bf8273bd0af5fb428130b6c30256e
Garrett <gwstallsmith@gmail.com>
2023-02-07 17:57:00 -0500

    Now logging to console BB84 sim values. Still buggy.

--- bb84 Website/bb84.js
+++ bb84 Website/bb84.js
@@ -1,46 +1,46 @@
 class Photon {
     constructor(bit, basis, polar){
-        this.bit_ = bit
-        this.basis_ = basis
-        this.polar_ = polar
+        this.bit_ = bit;
+        this.basis_ = basis;
+        this.polar_ = polar;
     }
 
-    getBit() { return this.bit_ }
-    getBasis() { return this.basis_ }
-    getPolar() { return this.polar_ }
+    getBit() { return this.bit_; }
+    getBasis() { return this.basis_; }
+    getPolar() { return this.polar_; }
 
-    setBit(newBit) { this.bit_ = newBit }
-    setBasis(newBasis) { this.basis_ = newBasis }
-    setPolar(newPolar) { this.polar_ = newPolar }
+    setBit(newBit) { this.bit_ = newBit; }
+    setBasis(newBasis) { this.basis_ = newBasis; }
+    setPolar(newPolar) { this.polar_ = newPolar; }
 }
 
 class Alice {
     constructor() {
-        this.bit_ = "1"
-        this.basis_ = "+"
-        this.polar_ = "0"
-        this.otp_ = []
-        this.botp_ = []
+        this.bit_ = "1";
+        this.basis_ = "+";
+        this.polar_ = "0";
+        this.otp_ = [];
+        this.botp_ = [];
     }
 
     randBitBasisPolar() {
         if(Math.random() % 2 == 0){
-            this.basis_ = "+"
+            this.basis_ = "+";
             if(Math.random() % 2 == 0) {
-                this.polar_ = "0"
-                this.bit_ = "1"
+                this.polar_ = "0";
+                this.bit_ = "1";
             } else {
-                this.polar_ = "90"
-                this.bit_ = "0"
+                this.polar_ = "90";
+                this.bit_ = "0";
             }
         } else {
             this.basis_ = "x"
             if(Math.random() % 2 == 0) {
-                this.polar_ = "45"
-                this.bit_ = "0"
+                this.polar_ = "45";
+                this.bit_ = "0";
             } else {
-                this.polar_ = "135"
-                this.bit_ = "1"
+                this.polar_ = "135";
+                this.bit_ = "1";
             }
         }
     }
@@ -48,7 +48,7 @@ class Alice {
     generateOTP(size) {
         for(let i = 0; i < size; ++i) {
             this.randBitBasisPolar()
-            this.otp_.push(Photon(this.bit_, this.basis_, this.polar_))
+            this.otp_.push(new Photon(this.bit_, this.basis_, this.polar_))
         }
         return this.otp_;
     }
@@ -56,7 +56,7 @@ class Alice {
     compareOTP(inOTP) {
         for(let i = 0; i < inOTP.length; ++i) {
             if(this.otp_[i] == inOTP[i]) {
-                botp_.push(otp[i].getBit())
+                this.botp_.push(inOTP[i].getBit());
             }
         }
         return this.botp_;
@@ -64,110 +64,110 @@ class Alice {
 
     printOTP() {
         for(let i = 0; i < this.otp_.length; ++i) {
-            console.log(i + ".) " + this.otp_[i].getBit() + " " + this.otp_[i].getBasis() + " " + this.otp_.getPolar())
+            console.log(i + ".) " + this.otp_[i].getBit() + " " + this.otp_[i].getBasis() + " " + this.otp_.getPolar());
         }
     }
 
     printBOTP() {
         for(let i = 0; i < this.botp_.length; ++i) {
-            console.log(i + ".) " + this.botp_[i])
+            console.log(i + ".) " + this.botp_[i]);
         }
     }
 }
 
 class Bob {
     constructor() {
-        this.bit_ = "1"
-        this.basis_ = "+"
-        this.polar_ = "0"
-        this.otp_ = []
+        this.bit_ = "1";
+        this.basis_ = "+";
+        this.polar_ = "0";
+        this.otp_ = [];
     }
 
     randBitBasisPolar() {
         if(Math.random() % 2 == 0){
-            this.basis_ = "+"
+            this.basis_ = "+";
             if(Math.random() % 2 == 0) {
-                this.polar_ = "0"
-                this.bit_ = "1"
+                this.polar_ = "0";
+                this.bit_ = "1";
             } else {
-                this.polar_ = "90"
-                this.bit_ = "0"
+                this.polar_ = "90";
+                this.bit_ = "0";
             }
         } else {
-            this.basis_ = "x"
+            this.basis_ = "x";
             if(Math.random() % 2 == 0) {
-                this.polar_ = "45"
-                this.bit_ = "0"
+                this.polar_ = "45";
+                this.bit_ = "0";
             } else {
-                this.polar_ = "135"
-                this.bit_ = "1"
+                this.polar_ = "135";
+                this.bit_ = "1";
             }
         }
     }
 
     measureOTP(inOTP) {
         for(let i = 0; i < inOTP.length; ++i) {
-            this.randBitBasisPolar()
+            this.randBitBasisPolar();
             if(inOTP[i].getBasis() == this.basis_ && inOTP[i].getPolar() == this.polar_) {
-                this.otp_.push(inOTP[i])
+                this.otp_.push(inOTP[i]);
             } else if(inOTP[i].getBasis() == this.basis_ && inOTP[i].getPolar() != this.polar_) {
                 if(inOTP[i].getPolar() == "0")
-                    this.otp_.push(Photon("1", "+", "0"))
+                    this.otp_.push(new Photon("1", "+", "0"));
                 
                 else if(inOTP[i].getPolar() == "45")
-                    this.otp_.push(Photon("0", "x", "45"))
+                    this.otp_.push(new Photon("0", "x", "45"));
 
                 else if(inOTP[i].getPolar() == "90")
-                    this.otp_.push(Photon("0", "+", "90"))
+                    this.otp_.push(new Photon("0", "+", "90"));
 
                 else if(inOTP[i].getPolar() == "135")
-                    this.otp_.push(Photon("1", "x", "134"))
+                    this.otp_.push(new Photon("1", "x", "134"));
             } else {
-                this.otp_.push(Photon("U", this.basis_, this.polar_))
+                this.otp_.push(new Photon("U", this.basis_, this.polar_));
             }
         }
-        return this.otp_s
+        return this.otp_;
     }
 
     printOTP() {
         for(let i = 0; i < this.otp_.length; ++i) {
-            console.log(i + ".) " + this.otp_[i].getBit() + " " + this.otp_[i].getBasis() + " " + this.otp_.getPolar())
+            console.log(i + ".) " + this.otp_[i].getBit() + " " + this.otp_[i].getBasis() + " " + this.otp_.getPolar());
         }
     }
 
     printBOTP() {
         for(let i = 0; i < this.botp_.length; ++i) {
-            console.log(i + ".) " + this.botp_[i])
+            console.log(i + ".) " + this.botp_[i]);
         }
     }
 }
 
 class Eve {
     constructor() {
-        this.bit_ = "1"
-        this.basis_ = "+"
-        this.polar_ = "0"
-        this.otp_ = []
+        this.bit_ = "1";
+        this.basis_ = "+";
+        this.polar_ = "0";
+        this.otp_ = [];
     }
 
     randBitBasisPolar() {
         if(Math.random() % 2 == 0){
-            this.basis_ = "+"
+            this.basis_ = "+";
             if(Math.random() % 2 == 0) {
-                this.polar_ = "0"
-                this.bit_ = "1"
+                this.polar_ = "0";
+                this.bit_ = "1";
             } else {
-                this.polar_ = "90"
-                this.bit_ = "0"
+                this.polar_ = "90";
+                this.bit_ = "0";
             }
         } else {
-            this.basis_ = "x"
+            this.basis_ = "x";
             if(Math.random() % 2 == 0) {
-                this.polar_ = "45"
-                this.bit_ = "0"
+                this.polar_ = "45";
+                this.bit_ = "0";
             } else {
-                this.polar_ = "135"
-                this.bit_ = "1"
+                this.polar_ = "135";
+                this.bit_ = "1";
             }
         }
     }
@@ -176,88 +176,105 @@ class Eve {
         for(let i = 0; i < inOTP.length; ++i) {
             this.randBitBasisPolar()
             if(inOTP[i].getBasis() == this.basis_ && inOTP[i].getPolar() == this.polar_) {
-                this.otp_.push(inOTP[i])
+                this.otp_.push(inOTP[i]);
             } else if(inOTP[i].getBasis() == this.basis_ && inOTP[i].getPolar() != this.polar_) {
                 if(inOTP[i].getPolar() == "0")
-                    this.otp_.push(Photon("1", "+", "0"))
+                    this.otp_.push(new Photon("1", "+", "0"));
                 
                 else if(inOTP[i].getPolar() == "45")
-                    this.otp_.push(Photon("0", "x", "45"))
+                    this.otp_.push(new Photon("0", "x", "45"));
 
                 else if(inOTP[i].getPolar() == "90")
-                    this.otp_.push(Photon("0", "+", "90"))
+                    this.otp_.push(new Photon("0", "+", "90"));
 
                 else if(inOTP[i].getPolar() == "135")
-                    this.otp_.push(Photon("1", "x", "134"))
+                    this.otp_.push(new Photon("1", "x", "134"));
             } else {
-                this.otp_.push(Photon("U", this.basis_, this.polar_))
+                this.otp_.push(new Photon("U", this.basis_, this.polar_));
             }
         }
-        return this.otp_s
+        return this.otp_
     }
 
     printOTP() {
         for(let i = 0; i < this.otp_.length; ++i) {
-            console.log(i + ".) " + this.otp_[i].getBit() + " " + this.otp_[i].getBasis() + " " + this.otp_.getPolar())
+            console.log(i + ".) " + this.otp_[i].getBit() + " " + this.otp_[i].getBasis() + " " + this.otp_.getPolar());
         }
     }
 
     printBOTP() {
         for(let i = 0; i < this.botp_.length; ++i) {
-            console.log(i + ".) " + this.botp_[i])
+            console.log(i + ".) " + this.botp_[i]);
         }
     }
 }
 
 class BB84 {
     constructor() {
-        this.a_ = Alice()
-        this.b_ = Bob()
-        this.e_ = Eve()
+        this.a_ = new Alice();
+        this.b_ = new Bob();
+        this.e_ = new Eve();
 
-        this.agreedOTP_ = []
+        this.agreedOTP_ = [];
 
-        this.errorRate_ = 0
+        this.errorRate_ = 0;
 
-        this.eveIntercept_ = false
-        this.eveDetect_ = false
+        this.eveIntercept_ = false;
+        this.eveDetect_ = false;
     }
 
     runProtocol(keySize, eveIntercept) {
         if(eveIntercept) {
-            this.agreedOTP_ = this.a_.compareOTP(this.b_.compareOTP(this.e_.measureOTP(this.a_.generateOTP(keySize))))
+            this.agreedOTP_ = this.a_.compareOTP(this.b_.measureOTP(this.e_.measureOTP(this.a_.generateOTP(keySize))));
             this.eveIntercept_ = true;
         } else 
-            this.agreedOTP_ = this.a_.compareOTP(this.b_.compareOTP(this.a_.generateOTP(keySize)))
+            this.agreedOTP_ = this.a_.compareOTP(this.b_.measureOTP(this.a_.generateOTP(keySize)));
 
-        this.errorRate_ = ((agreedOTP.length * 100) / keySize)
+
+        this.errorRate_ = ((this.agreedOTP_.length * 100) / keySize);
 
         if(this.errorRate_ < 40)
-            this.eveDetect_ = true
+            this.eveDetect_ = true;
     }
 
-    getErrorRate() { return this.errorRate_ }
-    getEveIntercept() { return this.eveIntercept_ }
-    getEveDetect() { return this.eveDetect_ }
-    getAgreedOTP() { return this.agreedOTP_ }
+    getErrorRate() { return this.errorRate_; }
+    getEveIntercept() { return this.eveIntercept_; }
+    getEveDetect() { return this.eveDetect_; }
+    getAgreedOTP() { return this.agreedOTP_; }
 }
 
 class SimManager {
-    constructor() {
-        this.simCount_ = 0
-        this.simList_ = []
+    constructor(simCount) {
+        this.simCount_ = simCount;
+        this.simList_ = [];
+        for(let i = 0; i < simCount; ++i) {
+            this.simList_.push(new BB84());
+        }
+
     }
 
     runSim(keySize, eveInterceptChance) {
+        for(let i = 0; i < this.simCount_; i++) {
+            //if(Math.random() % eveInterceptChance == 0) {
+            //    this.simList_[i].runProtocol(keySize, true);
+            //} else {
+                this.simList_[i].runProtocol(keySize, false)
+            //}
+        }
+    }
+
+    simResults() {
         for(let i = 0; i < this.simCount_; ++i) {
-            if(Math.random() % eveInterceptChance == 0) {
-                console.log("Sim #" + i)
-                console.log("Error Rate: " + this.simList[i].getErrorRate())
-                console.log("Eve Intercept: " + this.simList_[i].getEveIntercept())
-                console.log("Eve Detection: " + this.simList_[i].getErrorRate() + "\n")
-            }
+            console.log("Sim #" + i);
+            console.log("Error Rate: " + this.simList_[i].getErrorRate());
+            console.log("Eve Intercept: " + this.simList_[i].getEveIntercept());
+            console.log("Eve Detection: " + this.simList_[i].getErrorRate() + "\n");
         }
     }
 }
 
-export{Photon, Alice, Bob, Eve, BB84, SimManager}
+function main() {
+    let s = new SimManager(10);
+    s.runSim(100, 2);
+    s.simResults()
+}
--- bb84 Website/main.js
+++ bb84 Website/main.js
@@ -1,1 +1,24 @@
-import * as bb84 from './bb84.js'
+import * as bb84 from "./bb84.js";
+
+//var phot = new bb84.Photon(0, '+', 90);
+let sim = new SimManger(10);
+sim.runSim(100, 2);
+
+sim.simResults();
+
+//console.log(phot.getBit());
+//console.log(phot.getBasis());
+//console.log(phot.getPolar());
+
+//document.addEventListener("DOMContentLoaded", () =>{
+////  var para = document.getElementById("test");
+////	var text = "The bit is: " + phot.getBit() + "\n";
+////	var text = text = text +  "The basis is : " + phot.getBasis() + "\n";
+////	var text = text + "The polarity is: " + phot.getPolar();
+////	para.innerText = text;
+//
+//    sim.runSim(100, 2);
+//
+//    sim.simResults();
+//
+//});
